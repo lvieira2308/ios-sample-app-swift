7822f7789d14f2ac22082d9991f41aaa237dc3c4
lvieira2308 <lvieira2308@gmail.com>
2025-01-30 00:02:49 +0000

    Initial Commit

---
+++ myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Luis Vieira on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myAppTests/myAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  myAppTests.swift
+//  myAppTests
+//
+//  Created by Luis Vieira on 30/01/25.
+//
+
+import Testing
+@testable import myApp
+
+struct myAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
